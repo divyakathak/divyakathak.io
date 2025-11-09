6b294f930affc82c0c03f5b4b218f9e59bc436b4
divya <snahlatavyas332212@gmail.com>
2025-11-10 01:01:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Hero from "./Components/Hero";
-import Events from "./Components/Events";
 import Classes from "./Components/Classes";
-import Photo from "./Components/Photo"; // Photos from src/Assets
+import Photo from "./Components/Photo";
+import Events from "./Components/Events";
 import Contact from "./Components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
-      <header className="bg-black text-white py-4 text-center text-3xl font-bold">
-        Dance Studio
-      </header>
-
-      {/* Sections */}
-      <main>
-        <section id="hero"><Hero /></section>
-        <section id="events"><Events /></section>
-        <section id="classes"><Classes /></section>
-        <section id="photo"><Photo /></section>
-        <section id="contact"><Contact /></section>
-      </main>
-
-      {/* Footer */}
-      <footer className="bg-black text-white text-center py-4 mt-8">
-        © 2025 Dance Studio. All rights reserved.
-      </footer>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <Classes />
+              <Photo />
+              <Events />
+              <Contact />
+            </>
+          }
+        />
+      </Routes>
+    </HashRouter>
   );
 }
 
